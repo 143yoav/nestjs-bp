374b65af14840be65a636b625d6d199a480c2af2
yoavavidan <143yoav@gmail.com>
2024-09-26 17:02:16 +0300

    auth module added

---
+++ apps/auth/src/users/dto/create-user.dto.ts
@@ -0,0 +1,17 @@
+import {
+  IsEmail,
+  IsNotEmpty,
+  IsString,
+  IsStrongPassword,
+} from 'class-validator';
+
+export class CreateUserDto {
+  @IsEmail()
+  @IsNotEmpty()
+  email: string;
+
+  @IsString()
+  @IsStrongPassword()
+  @IsNotEmpty()
+  password: string;
+}
